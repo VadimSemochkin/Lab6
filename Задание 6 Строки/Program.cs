ced7a642fb8f544cb0e0ff911b2a4686e85953c8
User <User@DESKTOP-VUOV3VD>
2021-11-28 19:00:17 +0300

    Добавьте файлы проекта.

---
+++ Задание 6 Строки/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6_Строки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string a = Console.ReadLine();
+            string[] str = a.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int max = 0, nomer = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length>max)
+                {
+                    max = str[i].Length;
+                    nomer = i;
+                }
+
+            }
+            Console.WriteLine("Самое длинное слово - {0}",str[nomer]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string str = Console.ReadLine();
+            string strLow = str.ToLower();
+            //Console.Write("предложение в нижнем регистре: ");
+            //Console.WriteLine(strLow);
+            
+            string[] ar = { " ", ",", ".", "!", "?", "-" };
+            for (int i = 0; i < ar.Length; i++)
+            {
+                strLow = strLow.Replace(ar[i], "");
+                
+            }
+            //Console.Write("предложение в нижнем регистре и без знаков: ");
+            //Console.WriteLine(strLow);
+
+            string reversStr = "";
+         
+            for (int i = strLow.Length-1; i>=0; i--)
+            {
+                reversStr += strLow[i];
+            }
+            //Console.WriteLine(reversStr);
+ 
+            if (strLow==reversStr)
+            {
+                Console.WriteLine("Введенная строка - палиндром.");
+            }
+            else
+
+                Console.WriteLine("Введенная строка не является палиндромом.");
+            Console.ReadKey();
+
+        }
+       
+       
+    }
+}
